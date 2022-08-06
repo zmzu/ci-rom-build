247964c636fe5292599571c52a2994a9b98f9151
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-06 16:40:14 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,11 +1,10 @@
 apt-get update -y
-apt-get install curl python3.8 -y
+apt-get install curl python3.8 git -y
 ln -s /usr/bin/python3.8 /usr/bin/python
 mkdir ~/bin
 PATH=~/bin:$PATH
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
-ls /use/bin/python*
 python --version
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-14.1 -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
