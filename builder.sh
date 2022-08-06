c262742097caa5e1874d6e12338acea1e10f6169
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-06 15:31:17 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,6 +1,10 @@
 #pacman -Syu --noconfirm --needed git python
 apt-get update -y
-apt-get install repo -y
+export REPO=$(mktemp /tmp/repo.XXXXXXXXX)
+curl -o ${REPO} https://storage.googleapis.com/git-repo-downloads/repo
+gpg --recv-key 8BB9AD793E8E6153AF0F9A4416530D5E920F5C65
+curl -s https://storage.googleapis.com/git-repo-downloads/repo.asc | gpg --verify - ${REPO} && install -m 755 ${REPO} ~/bin/repo
+#apt-get install repo -y
 #sudo curl --create-dirs -L -o /usr/local/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
 #sudo chmod a+rx /usr/local/bin/repo
 #wget https://raw.githubusercontent.com/akhilnarang/scripts/master/setup/arch-manjaro.sh && ./arch-manjaro.sh
