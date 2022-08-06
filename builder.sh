1dda022bca403b1aa63e199f44f0770cdc2f02d3
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-06 15:39:38 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,9 +1,9 @@
 #pacman -Syu --noconfirm --needed git python
 apt-get update -y
-apt-get install repo python3 -y
-python --version
-#sudo curl --create-dirs -L -o /usr/local/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
-#sudo chmod a+rx /usr/local/bin/repo
-#wget https://raw.githubusercontent.com/akhilnarang/scripts/master/setup/arch-manjaro.sh && ./arch-manjaro.sh
-python3 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-14.1 -g default,-mips,-darwin,-notdefault
+#apt-get install repo python3 -y
+export REPO=$(mktemp /tmp/repo.XXXXXXXXX)
+curl -o ${REPO} https://storage.googleapis.com/git-repo-downloads/repo
+gpg --recv-key 8BB9AD793E8E6153AF0F9A4416530D5E920F5C65
+curl -s https://storage.googleapis.com/git-repo-downloads/repo.asc | gpg --verify - ${REPO} && install -m 755 ${REPO} ~/bin/repo
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-14.1 -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
