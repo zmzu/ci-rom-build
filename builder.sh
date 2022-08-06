f06c38a279d31557afe24ab22944d29e89b2fcfa
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-06 15:53:07 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,5 +1,6 @@
 #pacman -Syu --noconfirm --needed git python
 apt-get update -y
 apt-get install tmate -y
+tmate
 #repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-14.1 -g default,-mips,-darwin,-notdefault
 #repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
