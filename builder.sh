89284607ff56dc833bc169f01b6d5d87d9381c2f
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-06 15:49:58 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,9 +1,5 @@
 #pacman -Syu --noconfirm --needed git python
 apt-get update -y
-apt-get install curl gnupg2 -y
-export REPO=$(mktemp /tmp/repo.XXXXXXXXX)
-curl -o ${REPO} https://storage.googleapis.com/git-repo-downloads/repo
-gpg --recv-key 8BB9AD793E8E6153AF0F9A4416530D5E920F5C65
-curl -s https://storage.googleapis.com/git-repo-downloads/repo.asc | gpg --verify - ${REPO} && install -m 755 ${REPO} ~/bin/repo
+apt-get install repo -y
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-14.1 -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
