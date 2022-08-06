fe754f58c32afc15e63622afd1c3389bf2d4736b
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-06 18:43:07 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,15 +1,18 @@
 
 apt-get update -y
-apt-get install git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig curl python3.8 schedtool -y
+apt-get install neofetch git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig curl python3.8 schedtool -y
 ln -s /usr/bin/python3.8 /usr/bin/python
 mkdir ~/bin
 PATH=~/bin:$PATH
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 python --version
-repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b cm-14.1 -g default,-mips,-darwin,-notdefault
-time repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-git clone https://github.com/RahifM/proprietary_vendor_xiaomi -b cm-14.1 --depth=1 vendor/xiaomi
-. build/env*
-lunch lineage_mido-userdebug
-brunch mido
+neofetch
+java -version
+javac -version
+#repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b cm-14.1 -g default,-mips,-darwin,-notdefault
+#time repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+#git clone https://github.com/RahifM/proprietary_vendor_xiaomi -b cm-14.1 --depth=1 vendor/xiaomi
+#. build/env*
+#lunch lineage_mido-userdebug
+#brunch mido
