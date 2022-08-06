95dddd8755ec9b6a04c7ea38647373c52cdde72e
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-06 18:36:34 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,6 +1,6 @@
 
 apt-get update -y
-apt-get install build-essential curl python3.8 schedtool git -y
+apt-get install git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig curl python3.8 schedtool -y
 ln -s /usr/bin/python3.8 /usr/bin/python
 mkdir ~/bin
 PATH=~/bin:$PATH
