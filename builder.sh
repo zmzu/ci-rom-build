261a46b1e9f05c8135adeb8158143156bf32b13d
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-06 16:17:37 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,5 +1,5 @@
 apt-get update -y
-apt-get install curl python -y
+apt-get install curl python3 -y
 mkdir ~/bin
 PATH=~/bin:$PATH
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
