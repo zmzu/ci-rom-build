36d7f801043201568285c72fa94a0199be60b82f
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-06 16:01:42 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,9 +1,6 @@
 #pacman -Syu --noconfirm --needed git python
-apt-get update && apt-get install -y locales && rm -rf /var/lib/apt/lists/* \
-	&& localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
-LANG en_US.utf8
-apt-get update -y
-apt-get install tmate -y
-tmate
+python --version
+#apt-get update -y
+#apt-get install tmate -y
 #repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-14.1 -g default,-mips,-darwin,-notdefault
 #repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
