bfd1b82dc0340920f0b1fd21b00162cc7c400de6
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-06 15:20:00 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -2,7 +2,7 @@
 #sudo curl --create-dirs -L -o /usr/local/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
 #sudo chmod a+rx /usr/local/bin/repo
 apt-get update -y
-apt-get install git -y
+apt-get install git build-essential -y
 #wget https://raw.githubusercontent.com/akhilnarang/scripts/master/setup/arch-manjaro.sh && ./arch-manjaro.sh
 
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-14.1 -g default,-mips,-darwin,-notdefault
