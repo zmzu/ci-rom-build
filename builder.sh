90fa5c70edcdc88c5b641c57422475a9d37be7ca
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-06 16:15:34 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -4,5 +4,6 @@ mkdir ~/bin
 PATH=~/bin:$PATH
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
+apt-get install python
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-14.1 -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
