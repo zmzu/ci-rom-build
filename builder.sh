362a6ed88f4ccaa1ceb474fcc05ff304010da882
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-06 05:49:51 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,2 +1,5 @@
+sudo curl --create-dirs -L -o /usr/local/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
+sudo chmod a+rx /usr/local/bin/repo
+
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-14.1 -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
