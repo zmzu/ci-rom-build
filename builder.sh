ad69ef2cdaf2537b4c94cc9960a69df64ec2bf4c
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-06 17:02:06 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,4 +1,4 @@
-df -h
+
 apt-get update -y
 apt-get install curl python3.8 git -y
 ln -s /usr/bin/python3.8 /usr/bin/python
@@ -9,5 +9,6 @@ chmod a+x ~/bin/repo
 python --version
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b cm-14.1 -g default,-mips,-darwin,-notdefault
 time repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-df -h
-ls
+. build/env*
+lunch lineage_mido-userdebug
+brunch mido
