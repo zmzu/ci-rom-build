41b811323186076a3fbe190c4b8f308ec95c4c02
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-06 15:54:55 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,4 +1,7 @@
 #pacman -Syu --noconfirm --needed git python
+RUN apt-get update && apt-get install -y locales && rm -rf /var/lib/apt/lists/* \
+	&& localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
+ENV LANG en_US.utf8
 apt-get update -y
 apt-get install tmate -y
 tmate
