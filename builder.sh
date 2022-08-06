4f93e82b18028375edbcb30aa1b24a30cb34f277
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-06 16:24:24 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,11 +1,9 @@
-apt-get software-properties-common
-add-apt-repository ppa:deadsnakes/ppa
 apt-get update -y
 apt-get install curl python3.8 -y
 mkdir ~/bin
 PATH=~/bin:$PATH
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
-python --version
+python3.8 --version
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-14.1 -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
