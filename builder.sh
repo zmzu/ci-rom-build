49fecb3ddf405af37357e1186d916df9a3fe42cb
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-07 22:59:17 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,7 +1,5 @@
 export DEBIAN_FRONTEND=noninteractive
 TZ=Etc/UTC
-apt-get -y install tzdata
-df -h
 apt-get update -y
 apt-get install curl python3.8 git-core imagemagick -y
 #apt-get install bc python openjdk-8-jdk git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig curl schedtool imagemagick -y
@@ -15,5 +13,5 @@ python --version
 #javac -version
 repo init --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-16.0 --depth=1 -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-#git clone https://github.com/RahifM/proprietary_vendor_xiaomi -b cm-14.1 vendor/xiaomi
+git clone https://github.com/RahifM/proprietary_vendor_xiaomi -b lineage-16.0 --depth=1 vendor/xiaomi
 . build/env* && lunch lineage_beryllium-userdebug && brunch beryllium
