bd24a17f545d6287a02b82c50765b37362c478ab
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-08 01:30:42 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,4 +1,4 @@
-#export DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND=noninteractive
 TZ=Etc/UTC
 time apt-get update -y
 time apt-get install python imagemagick bc git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig -y
