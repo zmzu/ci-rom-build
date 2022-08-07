bdbf5ebaa3b2ef6b60dd8ca303edb82c4a828ccd
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-07 20:00:45 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,13 +1,13 @@
 df -h
 apt-get update -y
-apt-get install curl -y
+apt-get install curl python -y
 #apt-get install bc python openjdk-8-jdk git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig curl schedtool imagemagick -y
 #ln -s /usr/bin/python3.8 /usr/bin/python
 mkdir ~/bin
 PATH=~/bin:$PATH
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
-#python --version
+python --version
 #java -version
 #javac -version
 repo init --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-16.0 --depth=1 -g default,-mips,-darwin,-notdefault
