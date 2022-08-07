b0ee5fb8cc8b7fb4e33f12d8e675dce345936bb7
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-07 20:04:07 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,6 +1,6 @@
 df -h
 apt-get update -y
-apt-get install curl python3.8 -y
+apt-get install curl python3.8 git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig -y
 #apt-get install bc python openjdk-8-jdk git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig curl schedtool imagemagick -y
 ln -s /usr/bin/python3.8 /usr/bin/python
 mkdir ~/bin
