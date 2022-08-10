42adfba73c27f8ca0ddbb6027c69eb9ccce0baaf
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-10 17:46:38 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -3,8 +3,6 @@ export USER=buildfarm
 TZ=Etc/UTC
 time apt-get update -y
 time apt-get install rsync wget openjdk-8-jdk python imagemagick bc git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig -y
-#apt-get install bc python openjdk-8-jdk git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig curl schedtool imagemagick -y
-#ln -s /usr/bin/python3.8 /usr/bin/python
 mkdir ~/bin
 PATH=~/bin:$PATH
 curl https://storage.googleapis.com/git-repo-downloads/repo-1 > ~/bin/repo
@@ -12,16 +10,15 @@ chmod a+x ~/bin/repo
 python --version
 java -version
 javac -version
-#git clone https://github.com/fabianonline/telegram.sh $HOME/telegram.sh
-#wget https://raw.githubusercontent.com/sabmohmaya/mido/staging/slmk-4.4/lineage-15.1/.telegram.sh
-#mv .telegram.sh $HOME/.telegram.sh
-#TG=$HOME/telegram.sh/telegram
-#cp /etc/java-8-openjdk/security/java.security $HOME/
-#$TG -f $HOME/java.security
+git clone https://github.com/fabianonline/telegram.sh $HOME/telegram.sh
+wget https://raw.githubusercontent.com/sabmohmaya/mido/staging/slmk-4.4/lineage-15.1/.telegram.sh
+mv .telegram.sh $HOME/.telegram.sh
+TG=$HOME/telegram.sh/telegram
 wget https://raw.githubusercontent.com/sabmohmaya/new/java/java.security
 cp java.security /etc/java-8-openjdk/security/java.security
 time repo init --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-15.1 --depth=1 -g default,-mips,-darwin,-notdefault
 time repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 time git clone https://github.com/RahifM/proprietary_vendor_xiaomi -b lineage-15.1 --depth=1 vendor/xiaomi
-#export USE_NINJA=false
 . build/env* && brunch mido
+ls out/target/product/mido
+$TG -f out/target/product/mido/lineage-15.1-*-UNOFFICIAL-mido.zip
