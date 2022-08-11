1e80437d4f0dd4d31d9834cbebe00e985a5e0f86
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-11 15:17:15 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -19,8 +19,8 @@ cp java.security /etc/java-8-openjdk/security/java.security
 time repo init --no-repo-verify -u https://github.com/LineageOS/android.git -b cm-14.1 --depth=1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/RahifM/local_manifests -b cm-14.1 --depth=1 .repo/local_manifests
 time repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-time git clone https://github.com/RahifM/proprietary_vendor_xiaomi -b cm-14.1 --depth=1 vendor/xiaomi
-. build/env* && brunch mido
-cd out/target/product/mido
-curl --upload-file lineage-14.1-20220811-UNOFFICIAL-mido.zip https://transfer.sh/lineage-14.1-20220811-UNOFFICIAL-mido.zip > tup.txt
-$TG -f tup.txt
+time git clone https://github.com/TheMuppets/proprietary_vendor_wingtech -b cm-14.1 --depth=1 vendor/wingtech
+. build/env* && brunch wt88047
+#cd out/target/product/mido
+#curl --upload-file lineage-14.1-20220811-UNOFFICIAL-mido.zip https://transfer.sh/lineage-14.1-20220811-UNOFFICIAL-mido.zip > tup.txt
+#$TG -f tup.txt
