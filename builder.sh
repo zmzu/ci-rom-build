c27a9b258848a12637259089e914ca0e56ebabe1
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-15 01:02:35 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -13,6 +13,6 @@ time repo init --no-repo-verify -u https://github.com/LineageOS/android.git -b l
 virtualenv -p python2 ~/python
 . ~/python/bin/activate
 python --version
-time repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+time repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j32
 #time git clone https://github.com/RahifM/proprietary_vendor_xiaomi -b lineage-16.0 --depth=1 vendor/xiaomi
 #. build/env* && brunch beryllium
