cd547b95e39005e28ed7adb176cdeba04f666c1f
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-14 15:09:52 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -22,7 +22,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 git clone https://github.com/RahifM/proprietary_vendor_xiaomi -b cm-14.1 --depth=1 vendor/xiaomi
 . build/env*
 repopick -t n-asb-2021-09 && repopick -t n-asb-2021-10 && repopick -t n-asb-2021-11 && repopick -t n-asb-2021-12 && repopick -t n-asb-2022-01 && repopick -t n-asb-2022-02 && repopick -t n-asb-2022-03 && repopick -t n-asb-2022-04 && repopick -t n-asb-2022-05 && repopick -t n-asb-2022-06 && repopick -t n-asb-2022-07 && repopick -t n-asb-2022-08
-ls build && cd build/make && wget https://raw.githubusercontent.com/kerneltoast/patcher/cm-14.1-dumpling/patches/build/0010-release-keys.patch && git apply 0010-release-keys.patch && cd ../..
+cd build && wget https://raw.githubusercontent.com/kerneltoast/patcher/cm-14.1-dumpling/patches/build/0010-release-keys.patch && git apply 0010-release-keys.patch && cd ../
 #brunch mido
 #cd out/target/product/mido
 #curl --upload-file lineage-14.1-20220811-UNOFFICIAL-mido.zip https://transfer.sh/lineage-14.1-20220811-UNOFFICIAL-mido.zip > tup.txt
