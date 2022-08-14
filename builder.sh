3bbf44a7c3b41538df762399d532de2bb8967dd7
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-14 23:19:20 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,8 +1,7 @@
 export DEBIAN_FRONTEND=noninteractive
 TZ=Etc/UTC
 time apt-get update -y
-time apt-get install virtualenv rsync python imagemagick bc git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig -y
-#apt-get install bc python openjdk-8-jdk git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig curl schedtool imagemagick -y
+time apt-get install virtualenv rsync python3.8 imagemagick bc git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig -y
 ln -s /usr/bin/python3.8 /usr/bin/python
 mkdir ~/bin
 PATH=~/bin:$PATH
@@ -12,9 +11,9 @@ python --version
 #java -version
 #javac -version
 time repo init --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-16.0 --depth=1 -g default,-mips,-darwin,-notdefault
-#virtualenv -p python2 ~/python
-#. ~/python/bin/activate
-#python --version
-time repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-time git clone https://github.com/RahifM/proprietary_vendor_xiaomi -b lineage-16.0 --depth=1 vendor/xiaomi
-. build/env* && brunch beryllium
+virtualenv -p python2 ~/python
+. ~/python/bin/activate
+python --version
+#time repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+#time git clone https://github.com/RahifM/proprietary_vendor_xiaomi -b lineage-16.0 --depth=1 vendor/xiaomi
+#. build/env* && brunch beryllium
