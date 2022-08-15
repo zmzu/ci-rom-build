b8e9274ec64d8139f7e804e223eb03ce31cdda17
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-15 21:48:26 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,7 +1,7 @@
 export DEBIAN_FRONTEND=noninteractive
 TZ=Etc/UTC
 time apt-get update -y
-time apt-get install virtualenv rsync python python3.8 imagemagick bc git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig -y
+time apt-get install wget virtualenv rsync python python3.8 imagemagick bc git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig -y
 mv /usr/bin/python $HOME
 ln -s /usr/bin/python3.8 /usr/bin/python
 mkdir ~/bin
@@ -9,6 +9,10 @@ PATH=~/bin:$PATH
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 python --version
+https://github.com/fabianonline/telegram.sh $HOME/telegram.sh
+wget https://raw.githubusercontent.com/sabmohmaya/mido/staging/slmk-4.4/lineage-15.1/.telegram.sh
+mv .telegram.sh $HOME/.telegram.sh
+TG=$HOME/telegram.sh/telegram
 time repo init --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-16.0 --depth=1 -g default,-mips,-darwin,-notdefault
 virtualenv -p python2 ~/python
 . ~/python/bin/activate
@@ -16,4 +20,7 @@ python --version
 time repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j12
 #time git clone https://github.com/RahifM/proprietary_vendor_xiaomi -b lineage-16.0 --depth=1 vendor/xiaomi
 . build/env* && time lunch lineage_beryllium-userdebug
+ls .repo
+cd .repo
+$TG -f roomservice*
 #brunch beryllium
