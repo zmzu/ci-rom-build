f633f54c1b2db48175066c0f972600a2dbc23f49
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-22 04:38:45 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,8 +1,8 @@
 df -h
 export DEBIAN_FRONTEND=noninteractive
 TZ=Etc/UTC
-time apt-get update -y
-time apt-get install wget virtualenv rsync python python3.8 imagemagick bc git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig -y
+apt-get update -y
+apt-get install wget virtualenv rsync python python3.8 imagemagick bc git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig -y
 mv /usr/bin/python $HOME
 ln -s /usr/bin/python3.8 /usr/bin/python
 mkdir ~/bin
@@ -14,16 +14,9 @@ git clone https://github.com/fabianonline/telegram.sh --depth=1 $HOME/telegram.s
 wget https://raw.githubusercontent.com/sabmohmaya/mido/staging/slmk-4.4/lineage-15.1/.telegram.sh
 mv .telegram.sh $HOME/.telegram.sh
 TG=$HOME/telegram.sh/telegram
-time repo init --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-16.0 --depth=1 -g default,-mips,-darwin,-notdefault
+repo init --no-repo-verify -u git://git.osdn.net/gitroot/android-x86/manifest.git -b pie-x86 --depth=1
 virtualenv -p python2 ~/python
 . ~/python/bin/activate
 python --version
-time repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j12
-time git clone https://github.com/LineageOS/android_device_xiaomi_beryllium -b lineage-16.0 --depth=1 device/xiaomi/beryllium
-time git clone https://github.com/LineageOS/android_device_xiaomi_sdm845-common -b lineage-16.0 --depth=1 device/xiaomi/sdm845-common
-time git clone https://github.com/LineageOS/android_kernel_xiaomi_sdm845 -b lineage-16.0 --depth=1 kernel/xiaomi/sdm845
-time git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-16.0 --depth=1 packages/resources/devicesettings
-time git clone https://github.com/RahifM/proprietary_vendor_xiaomi -b lineage-16.0 --depth=1 vendor/xiaomi
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j12
 . build/env*
-#time lunch lineage_beryllium-userdebug
-brunch beryllium
