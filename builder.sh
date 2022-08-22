c762ef3496a9efb39700d5091d1c46c776892066
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-08-22 06:49:14 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -19,6 +19,8 @@ virtualenv -p python2 ~/python
 . ~/python/bin/activate
 python --version
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j12
+rm -rf kernel
+git clone https://github.com/RahifM/android-x86-kernel -b android-x86-9.0-r2 --depth=1 kernel 
 . build/env*
 lunch android_x86_64-userdebug
 make -j$(nproc --all) kernel
