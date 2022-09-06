111ec85872a36b5c273102830138d10896756717
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-09-07 00:45:09 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,8 +1,8 @@
 export DEBIAN_FRONTEND=noninteractive
 export USER=buildfarm
-TZ=Etc/UTC
-time apt-get update -y
-time apt-get install rsync wget openjdk-8-jdk python imagemagick bc git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig -y
+export TZ='Asia/Kolkata'
+apt-get update -y
+apt-get install rsync wget openjdk-8-jdk python imagemagick bc git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig -y
 mkdir ~/bin
 PATH=~/bin:$PATH
 curl https://storage.googleapis.com/git-repo-downloads/repo-1 > ~/bin/repo
@@ -14,12 +14,13 @@ git clone https://github.com/fabianonline/telegram.sh $HOME/telegram.sh
 wget https://raw.githubusercontent.com/sabmohmaya/mido/staging/slmk-4.4/lineage-15.1/.telegram.sh
 mv .telegram.sh $HOME/.telegram.sh
 TG=$HOME/telegram.sh/telegram
-wget https://raw.githubusercontent.com/sabmohmaya/new/java/java.security
+wget https://raw.githubusercontent.com/RahifM/repo_update/java/java.security
 cp java.security /etc/java-8-openjdk/security/java.security
-time repo init --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-15.1 --depth=1 -g default,-mips,-darwin,-notdefault
-time repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-time git clone https://github.com/RahifM/proprietary_vendor_xiaomi -b lineage-15.1 --depth=1 vendor/xiaomi
-. build/env* && brunch mido
+repo init --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-15.1 --depth=1 -g default,-mips,-darwin,-notdefault
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+git clone https://github.com/RahifM/proprietary_vendor_xiaomi -b lineage-15.1 --depth=1 vendor/xiaomi
+cd build/make && wget https://raw.githubusercontent.com/RahifM/repo_update/lineage-15.1-patches/relkey.patch && git apply relkey.patch && cd ../..
+. build/env* && lunch lineage_mido-user && make -j$(nproc --all) bacon
 cd out/target/product/mido
-curl --upload-file lineage-15.1-20220810-UNOFFICIAL-mido.zip https://transfer.sh/lineage-15.1-20220810-UNOFFICIAL-mido.zip > tup.txt
+curl --upload-file lineage-15.1-20220906-UNOFFICIAL-mido.zip https://transfer.sh/lineage-15.1-20220906-UNOFFICIAL-mido.zip > tup.txt
 $TG -f tup.txt
