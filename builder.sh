37145319b26f55d481d138c31fbe4df4b88cbb44
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-10-23 00:10:39 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -12,9 +12,8 @@ wget https://raw.githubusercontent.com/sabmohmaya/mido/staging/slmk-4.4/lineage-
 mv .telegram.sh $HOME/.telegram.sh
 TG=$HOME/telegram.sh/telegram
 time repo init --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 --depth=1 -g default,-mips,-darwin,-notdefault
+time git clone https://github.com/RahifM/local_manifests -b lineage-18.1-beryllium --depth 1 .repo/local_manifests
 time repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j12
-git clone https://gitlab.com/the-muppets/proprietary_vendor_xiaomi -b lineage-18.1 --depth=1 vendor/xiaomi
-time cd build/make && wget https://raw.githubusercontent.com/RahifM/repo_update/lineage-18.1-patches/relkey.patch && git apply relkey.patch && cd ../..
 . build/env*
 time lunch lineage_beryllium-user
 #time mka bacon
