5c3d8022271e5704ab014591b9b51791e20ab461
sabmohmaya <86376823+sabmohmaya@users.noreply.github.com>
2022-11-15 18:57:31 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -2,8 +2,8 @@ set -e
 df -h
 export DEBIAN_FRONTEND=noninteractive
 TZ=Etc/UTC
-time apt-get update -y
-time apt-get install wget virtualenv rsync python python3.8 imagemagick bc git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils libssl-dev xsltproc unzip fontconfig -y
+time sudo apt-get update -y
+time sudo apt-get install wget virtualenv rsync python python3.8 imagemagick bc git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils libssl-dev xsltproc unzip fontconfig -y
 mkdir ~/bin
 PATH=~/bin:$PATH
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
