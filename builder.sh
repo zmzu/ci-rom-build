21e563405ee4cddad512d56521b4e4933712fc0e
zmzu <134157207+zmzu@users.noreply.github.com>
2023-05-20 23:56:05 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -29,7 +29,12 @@ git clone https://github.com/TheMuppets/proprietary_vendor_wingtech -b cm-14.1 -
 repopick -t n-asb-2021-09 -f && repopick -t n-asb-2021-10 -f && repopick -t n-asb-2021-11 -f && repopick -t n-asb-2021-12 -f && repopick -t n-asb-2022-01 -f && repopick -t n-asb-2022-02 -f && repopick -t n-asb-2022-03 -f && repopick -t n-asb-2022-04 -f && repopick -t n-asb-2022-05 -f && repopick -t n-asb-2022-06 -f && repopick -t n-asb-2022-07 -f && repopick -t n-asb-2022-08 -f #&& repopick -t n-asb-2022-09
 cd build && wget https://raw.githubusercontent.com/kerneltoast/patcher/cm-14.1-dumpling/patches/build/0010-release-keys.patch && git apply 0010-release-keys.patch && cd ../
 cd external/iw && wget https://raw.githubusercontent.com/RahifM/repo_update/cm-14.1-patches/iw.patch && git apply iw.patch && cd ../..
-brunch wt88047
+git clone https://github.com/LineageOS/android_device_wingtech_wt88047 -b cm-14.1 --depth=1 device/wingtech/wt88047
+git clone https://github.com/LineageOS/android_device_cyanogen_msm8916-common -b cm-14.1 --depth=1 device/cyanogen/msm8916-common
+git clone https://github.com/LineageOS/android_kernel_wingtech_msm8916 -b cm-14.1 --depth=1 kernel/wingtech/msm8916
+git clone https://github.com/LineageOS/android_external_stlport -b cm-14.1 --depth=1 external/stlport
+git clone https://github.com/LineageOS/android_device_qcom_common -b cm-14.1 --depth=1 device/qcom/common
+lunch lineage_wt88047
 cd device/wingtech/wt88047 && wget https://raw.githubusercontent.com/RahifM/repo_update/cm-14.1-patches/wtfp.patch && git apply wtfp.patch && cd ../../..
 #make -j$(nproc --all) bacon
 cd out/target/product/wt88047
