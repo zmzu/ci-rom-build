932c52abe3b36e5a1db9574dda11e15401e6bb5b
zmzu <134157207+zmzu@users.noreply.github.com>
2023-05-20 23:02:07 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -26,11 +26,10 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 df -h
 git clone https://github.com/TheMuppets/proprietary_vendor_wingtech -b cm-14.1 --depth=1 vendor/wingtech
 . build/env*
-repopick -t n-asb-2021-09 -f
-#&& repopick -t n-asb-2021-10 && repopick -t n-asb-2021-11 && repopick -t n-asb-2021-12 && repopick -t n-asb-2022-01 && repopick -t n-asb-2022-02 && repopick -t n-asb-2022-03 && repopick -t n-asb-2022-04 && repopick -t n-asb-2022-05 && repopick -t n-asb-2022-06 && repopick -t n-asb-2022-07 && repopick -t n-asb-2022-08 && repopick -t n-asb-2022-09
+repopick -t n-asb-2021-09 -f && repopick -t n-asb-2021-10 -f && repopick -t n-asb-2021-11 -f && repopick -t n-asb-2021-12 -f && repopick -t n-asb-2022-01 -f && repopick -t n-asb-2022-02 -f && repopick -t n-asb-2022-03 -f && repopick -t n-asb-2022-04 -f && repopick -t n-asb-2022-05 -f && repopick -t n-asb-2022-06 -f && repopick -t n-asb-2022-07 -f && repopick -t n-asb-2022-08 -f #&& repopick -t n-asb-2022-09
 cd build && wget https://raw.githubusercontent.com/kerneltoast/patcher/cm-14.1-dumpling/patches/build/0010-release-keys.patch && git apply 0010-release-keys.patch && cd ../
 cd external/iw && wget https://raw.githubusercontent.com/RahifM/repo_update/cm-14.1-patches/iw.patch && git apply iw.patch && cd ../..
-lunch lineage_wt88047-user
+lunch lineage_wt88047
 cd device/wingtech/wt88047 && wget https://raw.githubusercontent.com/RahifM/repo_update/cm-14.1-patches/wtfp.patch && git apply wtfp.patch && cd ../../..
 #make -j$(nproc --all) bacon
 cd out/target/product/wt88047
