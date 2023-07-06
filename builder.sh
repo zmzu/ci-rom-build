b2d72c9d3c6c39bf4beca9b6318f20e58db58f53
zmzu <134157207+zmzu@users.noreply.github.com>
2023-07-06 20:49:58 +0000

    test

--- builder.sh
+++ builder.sh
@@ -23,26 +23,28 @@ git config --global color.ui false
 git config --global user.name RahifM
 git config --global user.email rahifmanjatha372@gmail.com
 repo init --no-repo-verify -u https://github.com/LineageOS/android.git -b cm-14.1 --depth=1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/RahifM/local_manifests --depth 1 -b cm-14.1 .repo/local_manifests
+
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 df -h
-git clone https://github.com/TheMuppets/proprietary_vendor_wingtech -b cm-14.1 --depth=1 vendor/wingtech
+#git clone https://github.com/TheMuppets/proprietary_vendor_wingtech -b cm-14.1 --depth=1 vendor/wingtech
 . build/env*
-repopick -t n-asb-2021-09 -f && repopick -t n-asb-2021-10 -f && repopick -t n-asb-2021-11 -f && repopick -t n-asb-2021-12 -f && repopick -t n-asb-2022-01 -f && repopick -t n-asb-2022-02 -f && repopick -t n-asb-2022-03 -f && repopick -t n-asb-2022-04 -f && repopick -t n-asb-2022-05 -f && repopick -t n-asb-2022-06 -f && repopick -t n-asb-2022-07 -f && repopick -t n-asb-2022-08 -f #&& repopick -t n-asb-2022-09
-cd build && wget https://raw.githubusercontent.com/kerneltoast/patcher/cm-14.1-dumpling/patches/build/0010-release-keys.patch && git apply 0010-release-keys.patch && cd ../
-cd external/iw && wget https://raw.githubusercontent.com/RahifM/repo_update/cm-14.1-patches/iw.patch && git apply iw.patch && cd ../..
-git clone https://github.com/LineageOS/android_device_wingtech_wt88047 -b cm-14.1 --depth=1 device/wingtech/wt88047
-git clone https://github.com/LineageOS/android_device_cyanogen_msm8916-common -b cm-14.1 --depth=1 device/cyanogen/msm8916-common
-git clone https://github.com/LineageOS/android_kernel_wingtech_msm8916 -b cm-14.1 --depth=1 kernel/wingtech/msm8916
-git clone https://github.com/LineageOS/android_external_stlport -b cm-14.1 --depth=1 external/stlport
-git clone https://github.com/LineageOS/android_device_qcom_common -b cm-14.1 --depth=1 device/qcom/common
+#repopick -t n-asb-2021-09 -f && repopick -t n-asb-2021-10 -f && repopick -t n-asb-2021-11 -f && repopick -t n-asb-2021-12 -f && repopick -t n-asb-2022-01 -f && repopick -t n-asb-2022-02 -f && repopick -t n-asb-2022-03 -f && repopick -t n-asb-2022-04 -f && repopick -t n-asb-2022-05 -f && repopick -t n-asb-2022-06 -f && repopick -t n-asb-2022-07 -f && repopick -t n-asb-2022-08 -f #&& repopick -t n-asb-2022-09
+#cd build && wget https://raw.githubusercontent.com/kerneltoast/patcher/cm-14.1-dumpling/patches/build/0010-release-keys.patch && git apply 0010-release-keys.patch && cd ../
+#cd external/iw && wget https://raw.githubusercontent.com/RahifM/repo_update/cm-14.1-patches/iw.patch && git apply iw.patch && cd ../..
+#git clone https://github.com/LineageOS/android_device_wingtech_wt88047 -b cm-14.1 --depth=1 device/wingtech/wt88047
+#git clone https://github.com/LineageOS/android_device_cyanogen_msm8916-common -b cm-14.1 --depth=1 device/cyanogen/msm8916-common
+#git clone https://github.com/LineageOS/android_kernel_wingtech_msm8916 -b cm-14.1 --depth=1 kernel/wingtech/msm8916
+#git clone https://github.com/LineageOS/android_external_stlport -b cm-14.1 --depth=1 external/stlport
+#git clone https://github.com/LineageOS/android_device_qcom_common -b cm-14.1 --depth=1 device/qcom/common
 lunch lineage_wt88047-user
 #brunch wt88047 > b.txt
 #$TG -f b.txt
-cd device/wingtech/wt88047 && wget https://raw.githubusercontent.com/RahifM/repo_update/cm-14.1-patches/wtfp.patch && git apply wtfp.patch && cd ../../..
+#cd device/wingtech/wt88047 && wget https://raw.githubusercontent.com/RahifM/repo_update/cm-14.1-patches/wtfp.patch && git apply wtfp.patch && cd ../../..
 #make -j$(nproc --all) bacon
 #cd out/target/product/wt88047
 #curl --upload-file lineage-14.1-20220906-UNOFFICIAL-wt88047.zip https://transfer.sh/lineage-14.1-20220906-UNOFFICIAL-wt88047.zip > tup.txt
 #$TG -f tup.txt
-ls -a
-du -hs *
-du -hs .repo
+#ls -a
+#du -hs *
+#du -hs .repo
