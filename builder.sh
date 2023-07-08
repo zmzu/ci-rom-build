342edf7dc5ed6560afd1adbf27b34739b60c46bc
zmzu <134157207+zmzu@users.noreply.github.com>
2023-07-08 14:33:29 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -16,13 +16,17 @@ mv .telegram.sh $HOME/.telegram.sh
 TG=$HOME/telegram.sh/telegram
 wget https://raw.githubusercontent.com/RahifM/repo_update/java/java.security
 cp java.security /etc/java-8-openjdk/security/java.security
+git config --global color.ui false
+git config --global user.name RahifM
+git config --global user.email rahifmanjatha372@gmail.com
 repo init --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-15.1 --depth=1 -g default,-mips,-darwin,-notdefault
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 git clone https://github.com/RahifM/proprietary_vendor_xiaomi -b lineage-15.1 --depth=1 vendor/xiaomi
 cd build/make && wget https://raw.githubusercontent.com/RahifM/repo_update/lineage-15.1-patches/relkey.patch && git apply relkey.patch && cd ../..
 . build/env*
 #brunch mido
-lunch lineage_mido-user && mka bacon
+lunch lineage_mido-user
+mka bacon
 #cd out/target/product/mido
 #curl --upload-file lineage-15.1-20220906-UNOFFICIAL-mido.zip https://transfer.sh/lineage-15.1-20220906-UNOFFICIAL-mido.zip > tup.txt
 #$TG -f tup.txt
