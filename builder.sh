5cea8e1f0eee1867549c329bb4e3bd9d60ca0178
zmzu <134157207+zmzu@users.noreply.github.com>
2023-07-08 16:15:44 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -4,8 +4,8 @@ export USER=buildfarm
 export TZ='Asia/Kolkata'
 sudo apt-get update -y
 sudo apt-get install rsync wget openjdk-8-jdk python imagemagick bc git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig -y
-mkdir ~/bin
-PATH=~/bin:$PATH
+#mkdir ~/bin
+#PATH=~/bin:$PATH
 curl https://storage.googleapis.com/git-repo-downloads/repo-1 > ~/bin/repo
 chmod a+x ~/bin/repo
 python --version
