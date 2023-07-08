e13701947fe216f6049b696da5f3c126e6150a38
zmzu <134157207+zmzu@users.noreply.github.com>
2023-07-08 16:04:16 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -8,7 +8,7 @@ time sudo apt-get install wget virtualenv rsync python2 imagemagick bc git-core 
 #ln -s /usr/bin/python3.8 /usr/bin/python
 #mkdir ~/bin
 #PATH=~/bin:$PATH
-curl https://storage.googleapis.com/git-repo-downloads/repo-1 > ~/bin/repo
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 python --version
 git clone https://github.com/fabianonline/telegram.sh --depth=1 $HOME/telegram.sh
@@ -23,10 +23,10 @@ time repo init --no-repo-verify -u https://github.com/LineageOS/android.git -b l
 #. ~/python/bin/activate
 #python --version
 time repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-#time git clone https://github.com/LineageOS/android_device_xiaomi_beryllium -b lineage-16.0 --depth=1 device/xiaomi/beryllium
-#time git clone https://github.com/LineageOS/android_device_xiaomi_sdm845-common -b lineage-16.0 --depth=1 device/xiaomi/sdm845-common
-#time git clone https://github.com/LineageOS/android_kernel_xiaomi_sdm845 -b lineage-16.0 --depth=1 kernel/xiaomi/sdm845
-#time git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-16.0 --depth=1 packages/resources/devicesettings
+time git clone https://github.com/LineageOS/android_device_xiaomi_beryllium -b lineage-16.0 --depth=1 device/xiaomi/beryllium
+time git clone https://github.com/LineageOS/android_device_xiaomi_sdm845-common -b lineage-16.0 --depth=1 device/xiaomi/sdm845-common
+time git clone https://github.com/LineageOS/android_kernel_xiaomi_sdm845 -b lineage-16.0 --depth=1 kernel/xiaomi/sdm845
+time git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-16.0 --depth=1 packages/resources/devicesettings
 time git clone https://github.com/RahifM/proprietary_vendor_xiaomi -b lineage-16.0 --depth=1 vendor/xiaomi
 . build/env*
 time lunch lineage_beryllium-user
