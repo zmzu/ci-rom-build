82fc862b14626cecee50b7de46a75e7a5b5c57de
zmzu <134157207+zmzu@users.noreply.github.com>
2023-07-08 14:40:19 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,8 +1,8 @@
 export DEBIAN_FRONTEND=noninteractive
 export USER=buildfarm
 export TZ='Asia/Kolkata'
-apt-get update -y
-apt-get install rsync wget openjdk-8-jdk python imagemagick bc git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig -y
+sudo apt-get update -y
+sudo apt-get install rsync wget openjdk-8-jdk python imagemagick bc git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig -y
 mkdir ~/bin
 PATH=~/bin:$PATH
 curl https://storage.googleapis.com/git-repo-downloads/repo-1 > ~/bin/repo
@@ -11,7 +11,7 @@ python --version
 java -version
 javac -version
 git clone https://github.com/fabianonline/telegram.sh $HOME/telegram.sh
-wget https://raw.githubusercontent.com/sabmohmaya/mido/staging/slmk-4.4/lineage-15.1/.telegram.sh
+wget https://raw.githubusercontent.com/zmzu/ci-rom-build/tgsh/.telegram.sh
 mv .telegram.sh $HOME/.telegram.sh
 TG=$HOME/telegram.sh/telegram
 wget https://raw.githubusercontent.com/RahifM/repo_update/java/java.security
