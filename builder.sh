b88d5acb6f1f0c3ef39b64e2fcf361197e7048f5
zmzu <134157207+zmzu@users.noreply.github.com>
2023-07-08 15:14:30 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -3,7 +3,7 @@ df -h
 export DEBIAN_FRONTEND=noninteractive
 export TZ='Asia/Kolkata'
 time sudo apt-get update -y
-time sudo apt-get install wget virtualenv rsync python python3.8 imagemagick bc git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig -y
+time sudo apt-get install wget virtualenv rsync python imagemagick bc git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig -y
 #mv /usr/bin/python $HOME
 #ln -s /usr/bin/python3.8 /usr/bin/python
 #mkdir ~/bin
