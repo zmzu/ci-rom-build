33d291d396bf27d3e68c9e3807e355a2f0bad364
zmzu <134157207+zmzu@users.noreply.github.com>
2023-07-10 20:58:36 +0530

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -19,16 +19,17 @@ git config --global color.ui false
 git config --global user.name RahifM
 git config --global user.email rahifmanjatha372@gmail.com
 time repo init --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-16.0 --depth=1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/RahifM/local_manifests --depth 1 -b lineage-16.0 .repo/local_manifestsgit clone https://github.com/RahifM/local_manifests --depth 1 -b lineage-16.0 .repo/local_manifests
 #virtualenv -p python2 ~/python
 #. ~/python/bin/activate
 #python --version
 time repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-time git clone https://github.com/LineageOS/android_device_xiaomi_beryllium -b lineage-16.0 --depth=1 device/xiaomi/beryllium
-time git clone https://github.com/LineageOS/android_device_xiaomi_sdm845-common -b lineage-16.0 --depth=1 device/xiaomi/sdm845-common
-time git clone https://github.com/LineageOS/android_kernel_xiaomi_sdm845 -b lineage-16.0 --depth=1 kernel/xiaomi/sdm845
-time git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-16.0 --depth=1 packages/resources/devicesettings
-time git clone https://github.com/RahifM/proprietary_vendor_xiaomi -b lineage-16.0 --depth=1 vendor/xiaomi
+#time git clone https://github.com/LineageOS/android_device_xiaomi_beryllium -b lineage-16.0 --depth=1 device/xiaomi/beryllium
+#time git clone https://github.com/LineageOS/android_device_xiaomi_sdm845-common -b lineage-16.0 --depth=1 device/xiaomi/sdm845-common
+#time git clone https://github.com/LineageOS/android_kernel_xiaomi_sdm845 -b lineage-16.0 --depth=1 kernel/xiaomi/sdm845
+#time git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-16.0 --depth=1 packages/resources/devicesettings
+#time git clone https://github.com/RahifM/proprietary_vendor_xiaomi -b lineage-16.0 --depth=1 vendor/xiaomi
 . build/env*
-time lunch lineage_beryllium-user
+time lunch lineage_beryllium-userdebug
 #mka bacon
 #brunch beryllium
